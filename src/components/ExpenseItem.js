7470ca5ab68f20039999f0c6dcbf08cc3908d9bf
alenslyn <alenslyn@gmail.com>
2023-05-07 14:14:43 +0300

    feat: add date and amount

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
 function ExpenseItem() {
-  return <h2>Expense item!</h2>;
+  return (
+    <div>
+      <div>March 28th 2021</div>
+      <div>
+        <h2>Car Insurance</h2>
+      </div>
+      <div>$294.67</div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
