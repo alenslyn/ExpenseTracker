d01b9923b9630c36fb4e60992c3c018cf0961837
alenslyn <alenslyn@gmail.com>
2023-05-08 12:08:28 +0300

    refactor: add new folders

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,5 +1,5 @@
 import "./Expenses.css";
-import Card from "./Card";
+import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 
 function Expenses(props) {
--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -2,6 +2,7 @@ import "./Card.css";
 
 function Card(props) {
   const classes = "card " + props.className;
+
   return <div className={classes}>{props.children}</div>;
 }
 
