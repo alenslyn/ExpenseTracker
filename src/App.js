e20228da76c8c017c0ba35e5136371bfb0ece457
alenslyn <alenslyn@gmail.com>
2023-05-13 18:19:20 +0300

    feat: add ExpensesFilter component

--- src/App.js
+++ src/App.js
@@ -24,9 +24,11 @@ const App = () => {
     },
   ];
 
+  const addExpenseHandler = (expense) => {};
+
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />;
     </div>
   );
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
@@ -45,6 +45,7 @@ const ExpenseForm = () => {
       amount: enteredAmount,
       date: new Date(enteredDate),
     };
+    props.onSaveExpenseData(expenseData);
     setEnteredTitle(""); //clear the input
     setEnteredAmount("");
     setEnteredDate("");
